25e906bbb8f87a3e07aef15b44c237ce4434e697
MfrankUg <muhindofrank2001@gmail.com>
2026-09-01 19:12:44 +0300

    initial commit for C

---
+++ main.c
@@ -0,0 +1,14 @@
+// introduction to the print function
+#include <stdio.h>  // preprocessor that included the input out library
+
+int main(){    // the entry point of our code
+    // printf -- outputs information 
+    // \n breaks to the next line
+    printf("Hello_world\n");
+    printf("My name is Jerry\n");
+    printf("I am 20 years old\n");
+    printf("I have dancing\n");
+
+}
+
+
