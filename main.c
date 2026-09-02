3c1913c5605ab3b6b6929fdce7848ea60a18e2ce
MfrankUg <muhindofrank2001@gmail.com>
2026-09-02 18:57:42 +0300

    practice assignment 1 added to the main.c file

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdio.h>  // preprocessor that included the input out library
 
 int main(){    // the entry point of our code
+    
+    printf("This is my first C");
     // printf -- outputs information 
     // \n breaks to the next line
     printf("Hello_world\n");
     printf("My name is Jerry\n");
     printf("I am 20 years old\n");
     printf("I have dancing\n");
+
+    return 0;
 
 }
 
