a9bf67226d69f33c6da802c6af6330a9055f7f2a
MfrankUg <muhindofrank2001@gmail.com>
2026-09-07 14:04:34 +0300

    changes

--- 03-variables-data-types/variables.c
+++ 03-variables-data-types/variables.c
@@ -13,8 +13,14 @@
     double roomBlock = 2.34; // this is known as a long float with a format specifier %lf
     char A = 'a';  // format specifier for character is %c
     float num1 = 3.14232434353452;
-    printf("the classroom block is %.2lf at level %c.", roomBlock , A );
-    printf("the num1 value is %f", num1);
+    printf("the classroom block is %.2lf at level %c. \n", roomBlock , A );
+    printf("the num1 value is %f\n", num1);
+   
+    // Implicit Type Conversion (Coercion)
 
+    int value1 = 13;
+    double value2 = 12.0;
+    double result = value1 + value2;
+    printf("the sum of %d + %.4lf is %.4lf", value1 , value2 , result);
  }
  
