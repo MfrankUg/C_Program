574178d056c9ad42b3f3a410ed34f1324cb1dfe9
MfrankUg <muhindofrank2001@gmail.com>
2026-09-07 07:24:59 +0300

    practice on the program structure

---
+++ 02-program-structure/program_structure.c
@@ -0,0 +1,10 @@
+// Program structure 
+#include <stdio.h> // This is a preprocessor that gives you acess the scanf() and printf() 
+
+int main(){ // Entry point of our program. 
+
+    // this is single line comment 
+    /* this is a multiple line comment */
+    printf("Hello_world");
+    return 0;
+}
