0eacde33f44ced4228cc7bb1a0dd7d4005f3bc6e
MfrankUg <muhindofrank2001@gmail.com>
2026-09-07 10:57:08 +0300

    practice on variables

---
+++ 03-variables-data-types/variables.c
@@ -0,0 +1,12 @@
+// handling variables
+ #include <stdio.h>
+
+ int main(){
+    int a; // variable initailization
+    a = 12; // variable declaration 
+    int age = 25; // variable initialization and declaration
+
+    printf("a = %d and age = %d", a , age);
+    
+ }
+ 
