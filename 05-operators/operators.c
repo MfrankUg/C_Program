038a2e2c8c8f7068428c02ddc27a41669b3fd86e
MfrankUg <muhindofrank2001@gmail.com>
2026-09-09 11:13:56 +0300

    changes

--- 05-operators/operators.c
+++ 05-operators/operators.c
@@ -8,5 +8,23 @@ int main(){
     printf("%d + %d = %d \n",num1,num2,add);
     // or 
     printf("%d + %d = %d \n", num1, num2, num1 + num2);
+    // Subtruction 
+    int a = 17;
+    int b = 5;
+    printf("17 - 5 = %d\n", a-b);
+    // Division 
+    int num3 = 17;
+    int num4 = 5;
+    printf("17 / 5 = %d\n", a/b);
+    // Modulus 
+    int num5 = 12;
+    int num6 = 4;
+    printf("12 %% 4 = %d\n", num5%num6);
+     // Modulus 
+    int num7 = 12;
+    int num8 = 4;
+    printf("12 * 4 = %d\n", num7*num8);
+   
+   
 
 }
