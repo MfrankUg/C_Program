3bd334e9ec7a44bfa3c8334d44a3da219d6dbbbf
MfrankUg <muhindofrank2001@gmail.com>
2026-09-10 19:51:48 +0300

    practice

---
+++ 05-operators/prac.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int x = 3;
+    x++;
+    printf(" if x =3 , x++: %d\n",x);
+    --x;
+    printf(" new x , --x: %d\n",x);
+     int y = 3;
+     ++y;
+    printf(" if y =3 ,++y: %d\n",y);
+    int z = 3;
+    z--;
+    printf(" if y =3 ,z--: %d\n",z);
+    int a = 3;
+    a--;
+    printf(" if a =3 ,a--: %d\n",a);
+    
+    
+
+}
