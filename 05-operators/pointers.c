322d27d71fda5b9c2173f4f7234132bd3a3a8060
MfrankUg <muhindofrank2001@gmail.com>
2026-09-10 21:13:14 +0300

    practice on pointers

---
+++ 05-operators/pointers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;
+    int *ptr = &age;          // ptr points to age
+
+    printf("Value of age      : %d\n", age);      // 25
+    printf("Address of age    : %p\n", &age);     // e.g. 0x7ffeeb...
+    printf("Value of ptr      : %p\n", ptr);      // same address
+    printf("Value via pointer : %d\n", *ptr);     // 25
+
+    *ptr = 30;                // change the value through the pointer
+    printf("New value of age  : %d\n", age);      // 30
+
+    return 0;
+}
