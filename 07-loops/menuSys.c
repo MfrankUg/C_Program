cc2ebc5fb29ee861df76a42c1072e5fdf8cd41dd
MfrankUg <muhindofrank2001@gmail.com>
2026-09-14 22:35:39 +0300

    added menuSys file for do while loop practice

---
+++ 07-loops/menuSys.c
@@ -0,0 +1,12 @@
+// vaild password menu using do while
+#include <stdio.h>
+int main(){
+int secretPin;
+scanf("%d",&secretPin);
+do{
+    printf("Enter 4 digit pin");
+   scanf("%d",&secretPin); 
+}while (secretPin == 1234);
+printf("Access Granted !!");
+return 0;
+}
