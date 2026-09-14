56bae75519a39b87ca31d692cddfecb590129081
MfrankUg <muhindofrank2001@gmail.com>
2026-09-14 22:40:09 +0300

    changes

--- 07-loops/menuSys.c
+++ 07-loops/menuSys.c
@@ -4,7 +4,7 @@ int main(){
 int secretPin;
 do{
     printf("Enter 4 digit pin");
-   scanf("%d",&secretPin); 
+    scanf("%d",&secretPin); 
 }while (secretPin == 1234);
 printf("Access Granted !!");
 return 0;
