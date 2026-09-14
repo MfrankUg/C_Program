3c2e83724cea2e4859938e68eacd00dc698c793a
MfrankUg <muhindofrank2001@gmail.com>
2026-09-14 21:49:21 +0300

    practice on loops

---
+++ 07-loops/loops.c
@@ -0,0 +1,13 @@
+// Practice on Loops
+// Simple counter
+#include <stdio.h>
+
+int main(){
+ int count = 0;
+ while (count <=9){
+     count ++;
+    printf("Number: %d\n",count);
+    // count ++;
+ }
+ return 0;
+}
