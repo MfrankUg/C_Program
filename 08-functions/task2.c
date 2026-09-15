52529882da1e1dd3db4a57715fdd38c67d8b25f4
MfrankUg <muhindofrank2001@gmail.com>
2026-09-15 07:23:53 +0300

    tasks 1 and 2

---
+++ 08-functions/task2.c
@@ -0,0 +1,12 @@
+/*
+Task 2: Maximum of Three Numbers (multiple parameters)
+Write a function that accepts three integer arguments and returns the largest one.
+
+Function Prototype: int find_max(int a, int b, int c);
+
+Logic: Compare a, b, and c using conditional statements (if...else) and return the maximum value.
+
+Task: Prompt the user or hardcode three numbers in main(), call find_max(), 
+and output the returned result.
+
+*/
---
+++ 08-functions/task3.c
@@ -0,0 +1,15 @@
+/*
+Task 3: Number Square & Cube (void functions & scope)
+Write a function that takes an integer, calculates both its square and cube, 
+and prints the result inside the function.
+
+Function Prototype: void print_powers(int num);
+
+Return Type: void (since it handles printing internally and returns no value).
+
+Task: Call print_powers(4) from main(). It should output:
+
+Output
+Number: 4 | Square: 16 | Cube: 64
+
+*/
