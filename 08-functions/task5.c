1b404c047a6aa662c7bc29d1702cc24ee50229cd
MfrankUg <muhindofrank2001@gmail.com>
2026-09-20 10:37:55 +0300

    task5

---
+++ 08-functions/task5.c
@@ -0,0 +1,12 @@
+/*
+Task 5:
+ Recursive Fibonacci Sequence 
+ Write a recursive function fibonacci(int n) that returns the n-th Fibonacci number.
+ Sequence Definition: 0, 1, 1, 2, 3, 5, 8, 13, 21,..... 
+ Fib(0) = 0
+ Fib(1) = 1
+ Fib(n) = Fib(n - 1) + Fib(n - 2) for n >= 2
+ Function Prototype:
+ int fibonacci(int n);
+ Expected Behavior: Calling fibonacci(7) should return 13.
+*/
