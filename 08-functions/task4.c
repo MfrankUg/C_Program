77ccf16fd4c2372de18459311ed5e38abde99881
MfrankUg <muhindofrank2001@gmail.com>
2026-09-20 10:07:50 +0300

    practice on recursive functions

--- 08-functions/task4.c
+++ 08-functions/task4.c
@@ -1,4 +1,16 @@
 /*
 Challenge: Sum of Digits (Recursive)
-Write a recursive function that takes a positive integer n and returns the sum of its individual digits.
+Write a recursive function that takes a positive integer n and returns the sum of its individual digits. // 1264
 */
+#include <stdio.h>
+int sum_of_integers(int num){
+    if(num < 10){
+        return num;
+      }
+    return (num % 10) + sum_of_integers(num/10);
+}
+
+int main(){
+   int result = sum_of_integers(1246);
+   printf("The sum of %d = %d", result);
+}
